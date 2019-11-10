8523d3417b0168aff16735bceb69766072f8eef0
Petri Luukkonen <petri.luukkonen@takamaki.fi>
2019-11-10 15:29:01 +0200

    uibuild

---
+++ build/precache-manifest.e7eeb27b102227fc58dc2c147755ad94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "701af03b65f3e5f7c565880910aba474",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b18a10b8cb0ff2d43925",
+    "url": "/static/css/main.3890fae7.chunk.css"
+  },
+  {
+    "revision": "9d7d7637ffdb3543cf9b",
+    "url": "/static/js/2.bf711040.chunk.js"
+  },
+  {
+    "revision": "b18a10b8cb0ff2d43925",
+    "url": "/static/js/main.1f46eef0.chunk.js"
+  },
+  {
+    "revision": "17d0ae5722a7a1d90858",
+    "url": "/static/js/runtime-main.86de38e2.js"
+  }
+]);
