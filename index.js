22ab272b54d5a504d2cce6c5f3fd257c30255dbb
Petri Luukkonen <petri.luukkonen@takamaki.fi>
2019-11-10 15:23:36 +0200

    uibuild

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
 
+app.use(express.static('build'))
 app.use(cors());
-
 app.use(bodyParser.json());
 
 morgan.token('showPayload', (req, res) => { if (!isEmpty(req.body)) {return JSON.stringify(req.body)} });
